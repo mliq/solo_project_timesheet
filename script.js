b558f0610b3f6b91ddb571e2ac89659070a3d3e7
mliq <liquori@gmail.com>
2015-04-24 13:19:17 -0500

    Re-wrote highlighting functionality from scratch so that regex could be used for greater accuracy

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var i = 0, j = 0;
-var namesArray = ["Erik", "Alicia", "Brian", "Casie", "Chelsea", "Clare", "Cody", "Jeanne", "Kaitlin", "Kelly", "Michael", "Luke", "Mary", "Aaron", "Michelle", "Rom", "Steve", "Terry", "Tracy", "Vince"];
+var i = 0, j = 0, str, pattern, addHighlight;
+var namesArray = ["Erik", "Alicia", "Brian", "Casie", "Chelsea", "Clare", "Cody", "Jeanne", "Kaitlin", "Kelly", "Michael", "Luke", "Mary", "Aaron", "Michelle", "Rom", "Steve", "Terry", "Tracy", "Vince", "KC", "Fred", "Scott", "Mark","Sherman","David","Joseph"];
 
 var quotesArray = ["Scott for sitting down with me today. Tracy, Vince for talking to me after I shared my story. They were supportive. ",
     "I want to thank the random Nerdery staff that stopped in to sprinkle some knowledge from their mighty brains on us.",
@@ -97,21 +97,33 @@ $(document).ready(function(){
         $('#quote').fadeOut(2000);
         // Delay 2 seconds for fading in new quote to allow fade out to transpire.
         window.setTimeout(function(){
-            // Add new quote hidden.
-            $('#results').delay(2000).html("<p style='display: none' id='quote'>" + quotesArray[i] + "</p>");
-            // Highlight any names.
-            for(j = 0; j < namesArray.length; j++){
-                $('#quote').highlight(namesArray[j]);
+            // MY HIGHLIGHT FUNCTION
+
+            // Store quote to string
+            str = quotesArray[i];
+
+            // Use regex to find names, if found add a span with class .highlight
+            for(j = 0; j < namesArray.length; j++) {
+                //str.replace(/(\b|^)rom/gi,"yay");
+                // Set search pattern from array[j]
+                pattern = new RegExp('\(\\b|^\)'+namesArray[j],'gi');
+                // Set what we will replace to add highlighing.
+                addHighlight = "<span class='highlight'>"+namesArray[j]+"</span>";
+                str = str.replace(pattern,addHighlight);
+                console.log(str);
             }
-            //console.log($( "p" ).text( arr.join( ", " ) ));
+            console.log(str);
+            // Add new quote hidden.
+            $('#results').delay(2000).html("<p style='display: none' id='quote'>" + str + "</p>");
+
             // Fade in new quote over 2 seconds.
             $('#quote').fadeIn(2000);
+
             // Increment index
             i++;
             // If index reaches end, reset to 0.
             if(i==quotesArray.length){i=0;}
         },2000);
     }, 6000);
-
 
 });
